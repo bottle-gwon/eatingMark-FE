3a9b670d3bc11f40edd48aeb309b08567cfa13b1
bottle-gwon <lazyguy1@daum.net>
2025-09-02 11:43:06 +0900

    🎨 Change getcurrentPosition to promise function

--- src/components/LoadingItem.jsx
+++ src/components/LoadingItem.jsx
@@ -3,7 +3,7 @@ export default function LoadingItem(){
     return(
         <div className="w-3/4">
             <h2>맛집 목록</h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 w-full bg-gray-400 py-5">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  w-full bg-gray-400 py-5">
                 {dummy.map((_, index)=>{
                     console.log('index')
                     return <div key={index} className="relative w-64 h-64 mx-10 my-2 overflow-hidden bg-gray-600">
